1052df4252a93bc06f3b0947e3d9a32493441892
Azure <157955050+Hediphone@users.noreply.github.com>
2024-05-02 19:14:01 +0800

    Add files via upload

---
+++ pages/PhpFunctions/connection.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $db_name = "dbms_sari_sari_store";
+
+    $conn = new mysqli($servername, $username, $password, $db_name );
+
+    if($conn->connect_error){
+        die("Connection failed".$conn->connect_error);
+    }
+
+    echo "";
+    
+?>
